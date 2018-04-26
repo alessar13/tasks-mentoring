6f6a2d59cdb3b10284d1882a55f764b0b95fd0ea
alessar <alex.chaly13@gmail.com>
2018-04-26 19:25:20 +0300

    1

--- Задание №1/home1.5.js
+++ Задание №1/home1.5.js
@@ -1,1 +1,23 @@
 //Сделайте функцию shuffle, которая параметром будет принимать массив и перемешивать его элементы в случайном порядке.
+
+function shuffle(array) 
+{
+    var result = [];
+    var length = array.length;
+    for (let i = 0; i < length; i++) 
+    {
+        var random = randomNumber(0, array.length - 1);
+        var num = array.splice(random,1)[0];
+        result.push(num);
+    }
+    return result;
+}
+
+
+function randomNumber(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(shuffle([1, 2, 3]));
+
